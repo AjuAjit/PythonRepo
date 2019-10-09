19bdb81cb660a2399f9e112a5935cf983486c419
AjuAjit <ajith.murugaian@gmail.com>
2019-10-09 23:32:55 +0530

    commiting illegal variable name program

---
+++ IllegalVariableName.py
@@ -0,0 +1,8 @@
+def illegal_variable_name():
+    1_a = "this is an invalid variable name"
+    print(1_a)
+    prod* = "you can't use operator in a variable name"
+    print(prod*)
+
+
+illegal_variable_name()
