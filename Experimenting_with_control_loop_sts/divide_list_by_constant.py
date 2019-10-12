0fb948e5dcb8f8ba9e2172df6f5344da34b7bb90
AjuAjit <ajith.murugaian@gmail.com>
2019-10-12 17:01:30 +0530

    commiting file

---
+++ Experimenting_with_control_loop_sts/divide_list_by_constant.py
@@ -0,0 +1,14 @@
+def divide_list_by_constant(n):
+    numbers = []
+    results = []
+    for i in range(1, n):
+        j=i
+        numbers.append(int(input()))
+        numbers[0]
+        print((numbers[i-1])/2)
+        #print(results[i])
+        results.append((numbers[i-1])/2)
+    print(results)
+
+
+divide_list_by_constant(4)
