c559024c42bf7b591e7062958182145c5031cd5a
AjuAjit <ajith.murugaian@gmail.com>
2019-10-12 17:24:42 +0530

    commiting division by constant

--- Experimenting_with_control_loop_sts/divide_list_by_constant.py
+++ Experimenting_with_control_loop_sts/divide_list_by_constant.py
@@ -1,14 +1,12 @@
+# divides the list by 2 and stores it in a new list
 def divide_list_by_constant(n):
-    numbers = []
-    results = []
-    for i in range(1, n):
-        j=i
+    numbers = []    # given list
+    results = []    # output list
+    for i in range(1, n+1):
         numbers.append(int(input()))
-        numbers[0]
-        print((numbers[i-1])/2)
-        #print(results[i])
         results.append((numbers[i-1])/2)
     print(results)
 
 
-divide_list_by_constant(4)
+no_of_terms = int(input("Enter the number of terms in the list: "))
+divide_list_by_constant(no_of_terms)    # pass the number of terms in the list as input
